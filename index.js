e0f18ab5d4667060a4e7012f896e49f96c28bb97
Zain1306 <zulabdin795@gmail.com>
2022-08-10 16:50:25 +0530

    new

--- index.js
+++ index.js
@@ -30,27 +30,34 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-let resobj={}
-app.get('/api/timestamp/:input',(req,res)=>{
- let input=req.params.input;
- if(input.includes('-')){
-  resobj['unix']=new Date(input).getTime();
-  resobj['utc']=new Date(input).toUTCString();
-}else{
-  input= parseInt(input);
-  resobj['unix']=new Date(input).getTime();
-  resobj['utc']=new Date(input).toUTCString();
-  }
-  if(!resobj['unix']||!resobj['utc']){
-    res.json({error:"Invalid Date"})
-  }
+let responseObject = {}
 
- res.json(resobj);
+app.get('/api/timestamp/:input', (request, response) => {
+  let input = request.params.input
+  
+  if(input.includes('-')){
+    /* Date String */
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }else{
+    /* Timestamp */
+    input = parseInt(input)
+    
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }
+  
+  if(!responseObject['unix'] || !responseObject['utc']){
+    response.json({error: 'Invalid Date'})
+  }
+  
+  
+  response.json(responseObject)
 })
 
-app.get('/api/timestamp',(res,rep)=>
-{
-  resobj['unix']=new Date().getTime();
-  resobj['utc']=new Date().toUTCString();
-  res.json(resobj);
+app.get('/api/timestamp', (request, response) => {
+  responseObject['unix'] = new Date().getTime()
+  responseObject['utc'] = new Date().toUTCString()
+  
+  response.json(responseObject)
 })
