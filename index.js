7408e810368c65af2a816e6c0e6a350fbdbeb75c
Zain1306 <zulabdin795@gmail.com>
2022-08-11 13:45:18 +0530

    new

--- index.js
+++ index.js
@@ -83,11 +83,11 @@ app.get('/api/:date?', (request, response) => {
   response.json(responseObject)
 })
 
-app.get("/ ", (req, res)=>{
+app.get(getUserInput('url') + '/api', (req, res)=>{
   let date = new Date();
-  let UTC = date.getTime();
+  let UTC = date.getTime()+20000;
   UTC = new Date(UTC);
   UTS = UTC.toUTCString();
-  let UNIX = date.getTime();
+  let UNIX = date.getTime()+20000;
   res.json({ unix: UNIX, utc: UTS });
 })
