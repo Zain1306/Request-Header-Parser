ba9c76ca8636a42ecb7d961a12594dd99bf432cb
Zain1306 <zulabdin795@gmail.com>
2022-08-11 13:10:21 +0530

    new

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ var listener = app.listen(process.env.PORT, function () {
 
 let responseObject = {}
 
-app.get('/api/timestamp/:date?', (request, response) => {
+app.get('/api/:date?', (request, response) => {
   let input = request.params.date
   
   if(input.includes(' ') || input.includes('-')){
@@ -83,7 +83,7 @@ app.get('/api/timestamp/:date?', (request, response) => {
   response.json(responseObject)
 })
 
-app.get('/api/timestamp', (request, response) => {
+app.get('/api/date?', (request, response) => {
   responseObject['unix'] = new Date().getTime()
   responseObject['utc'] = new Date().toUTCString()
   
